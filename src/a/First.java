597cd9b33ea764f45bc9b5cb62a8de5987e879f6
anjaemun <yedo0111@gmail.com>
2023-11-29 15:39:16 +0900

    first commit

---
+++ src/a/First.java
@@ -0,0 +1,12 @@
+package a; //현재 클래스가 소속된 패키지명
+
+public class First {	//현재 클래스명
+
+	public static void main(String[] args) {//First 클래스 블록 시작점
+		// main 메서드 (프로그램 시작 지점)
+	// TODO Auto-generated method stub
+		//실행 내용 작성되는 공간
+		System.out.println("바보멍청이");
+	}
+
+} //First 클래스 블록 끝점
